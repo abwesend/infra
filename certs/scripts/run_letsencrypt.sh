d2384b21196d74f6bb71afccbf41e7716cff3d08
beanieboi <beanie@benle.de>
2016-07-09 16:16:35 -0400

    update certbot script

--- certs/scripts/run_letsencrypt.sh
+++ certs/scripts/run_letsencrypt.sh
@@ -1,18 +1,15 @@
 for d in $DOMAINS
 do
- echo "Running letsencrypt for $d"
- letsencrypt --standalone --standalone-supported-challenges\
-  http-01 --agree-dev-preview --agree-tos --renew-by-default\
-  --server https://acme-v01.api.letsencrypt.org/directory\
-  --email $EMAIL -d $d certonly
+ echo "Running certbot for $d"
+ certbot certonly --standalone --agree-tos -n --email $EMAIL -d $d
  ec=$?
- echo "letsencrypt exit code $ec"
+ echo "certbot exit code $ec"
  if [ $ec -eq 0 ]
  then
   # For haproxy, you need to concatenate the full chain with the private key
-  cat /etc/letsencrypt/live/$d/fullchain.pem /etc/letsencrypt/live/$d/privkey.pem > /certs/$d.pem
+  # cat /etc/letsencrypt/live/$d/fullchain.pem /etc/letsencrypt/live/$d/privkey.pem > /certs/$d.pem
   # For nginx or apache, you need both separate files
-  # cp /etc/letsencrypt/live/$d/fullchain.pem /certs/$d.pem
-  # cp /etc/letsencrypt/live/$d/privkey.pem /certs/$d.key
+  cp /etc/letsencrypt/live/$d/fullchain.pem /certs/$d.pem
+  cp /etc/letsencrypt/live/$d/privkey.pem /certs/$d.key
  fi
 done
